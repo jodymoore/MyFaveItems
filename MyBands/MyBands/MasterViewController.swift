4496c14cec91e2bac3ba598c99466315b27ae096
jodymoore <jodymoore@g.harvard.edu>
2018-09-17 17:57:34 -0500

    Add files via upload
    initial commit

---
+++ MyBands/MyBands/MasterViewController.swift
@@ -0,0 +1,84 @@
+//
+//  MasterViewController.swift
+//  MyBands
+//
+//  Created by cis290 on 9/17/18.
+//  Copyright © 2018 Rock Valley College. All rights reserved.
+//
+
+import UIKit
+
+class MasterViewController: UITableViewController {
+    
+    var detailViewController: DetailViewController? = nil
+
+    var listArray = NSMutableArray()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        //********3) Add next two lines for array and title
+        listArray = ["Foo Fighters","Alice in Chains","Mettalica","Brooks & Dunn", "Nine inch Nails","Sound Garden", "Filter","Van-Halen"]
+        title = "My Favorite Bands"
+        if let split = splitViewController {
+            let controllers = split.viewControllers
+            detailViewController = (controllers[controllers.count-1] as! UINavigationController).topViewController as? DetailViewController
+        }
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        clearsSelectionOnViewWillAppear = splitViewController!.isCollapsed
+        super.viewWillAppear(animated)
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    
+    // MARK: - Segues
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "showDetail" {
+            if let indexPath = tableView.indexPathForSelectedRow {
+                //********5) Update below 6 lines of code
+                let object = listArray[indexPath.row] as! String
+                let controller = (segue.destination as! UINavigationController).topViewController as! DetailViewController
+                controller.detailItem = (object as String) + ".jpg"
+                controller.navigationItem.leftBarButtonItem = splitViewController?.displayModeButtonItem
+                controller.navigationItem.leftItemsSupplementBackButton = true
+                self.splitViewController?.preferredDisplayMode = .primaryHidden
+            }
+        }
+    }
+    
+    // MARK: - Table View
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+
+        return listArray.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+
+        let object = listArray[indexPath.row] as! NSString
+        cell.textLabel!.text = object.description
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+
+        return false
+    }
+
+    
+}
+
